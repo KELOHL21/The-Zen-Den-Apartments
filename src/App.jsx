bc8959d6f00880c574c0cc3703cc27d527d2f976
Kelsy <kelsyohlsson@gmail.com>
2024-03-28 08:23:06 +0200

    Awesome navbar done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+
+
+function App() {
+ 
+
+  return (
+    <div className="appBody">
+        <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,106 @@
+// import { Fragment } from 'react'
+import { Disclosure,  } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Home', href: '#', current: true },
+  { name: 'Apartments', href: '#', current: false },
+  { name: 'About Us', href: '#', current: false },
+  { name: 'Blog', href: '#', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
+  return (
+    <Disclosure as="nav" className="bg-[#D2D6D9]">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center p-2 text-gray-600">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="h-7 w-7" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className=" h-7 w-7" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              
+              <div className="flex flex-1  items-center justify-center  sm:justify-start">
+              <div className="absolute inset-y-0 right-0 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 flex">
+                  <img
+                    className="h-12 w-auto"
+                    src="/public/assets/logo.png"
+                    alt="Your Company"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-[#768692] text-white ' : ' hover:bg-gray-500 hover:text-white text-gray-600',
+                          'rounded-md px-3 py-2 text-sm font-medium text-gray-600'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+
+              <div className="absolute inset-y-0 right-0 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 hidden sm:flex">
+                <button
+                  type="button"
+                  className="relative text-gray-600 border-2 font-medium border-gray-500 px-5 py-1 rounded-sm"
+                >
+                  Contact 
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* This is the drop down menu */}
+         <Disclosure.Panel className="sm:hidden flex flex-col items-center m-auto ">
+            <div className="space-y-1 px-2 pb-10 pt-[8rem] items-center m-auto flex flex-col justify-center">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-500 w-full  text-white text-[27px] items-center m-auto flex flex-row justify-center' : 'text-gray-600 text-[27px] ',
+                    'block rounded-md px-3 py-6 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+
+              <div className='pt-[7rem] md:pt-[12rem]'>
+                <button
+                    type="button"
+                    className="relative px-9 py-2 text-gray-600 border-2 border-gray-500 rounded-sm">
+                    Contact
+                </button>
+              </div>
+
+            </div>
+          </Disclosure.Panel> 
+        </>
+      )}
+    </Disclosure>
+  )
+}
